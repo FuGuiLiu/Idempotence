de9c06f522ec2a2a5f35b747ef89353868a4b42e
ryder <liu997121@gmail.com>
2021-11-19 16:07:04 +0800

    防重token幂等性测试

---
+++ AntiWeightToken/src/main/java/com/sky/idea/antiweighttoken/AntiweighttokenApplication.java
@@ -0,0 +1,13 @@
+package com.sky.idea.antiweighttoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiweighttokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntiweighttokenApplication.class, args);
+	}
+
+}
---
+++ AntiWeightToken/src/test/java/com/sky/idea/antiweighttoken/AntiweighttokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.idea.antiweighttoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiweighttokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
